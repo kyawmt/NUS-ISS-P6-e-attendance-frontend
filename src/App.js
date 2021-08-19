83eb6f40e68fb7ba7f06eb581897fd80face6c05
larrysimm <sim.larry88@gmail.com>
2021-08-19 16:52:09 +0800

    1. Link the Admin Main Pageto various applications 2. Link Lecturer Main Page to various applications 3. Edit the classname=card to card icon 4. Temporary "comment" out the materialize.css file in order to display the checkbox and dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,61 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
-import FooterComponent from './components/fragments/FooterComponent';
-import HeaderComponent from './components/fragments/HeaderComponent';
-import LoginComponent from './components/fragments/LoginComponent';
-import ListLecturer from './components/admin/ListLecturer';
-import AddOrUpdateLecturer from './components/admin/AddOrUpdateLecturer';
-import AddOrUpdateModule from './components/admin/AddOrUpdateModule';
-import ListModule from './components/admin/ListModule';
-import AdminMainPageComponent from './components/admin/AdminMainPageComponent';
-import LecturerMainPage from './components/lecturer/LecturerMainPage';
-import HomeComponent from './components/fragments/HomeComponent';
-import ListStudent from './components/admin/ListStudent';
-import ListClassSchedule from './components/admin/ListClassSchedule';
-import ListClass from './components/admin/ListClass';
-import ListStudentsLeave from './components/admin/ListStudentsLeave';
-import ListModuleAttendence from './components/admin/ListModuleAttendance';
-import ViewModuleStudentAttendance from './components/admin/ViewModuleStudentAttendance';
-import ViewModuleAttendence from './components/admin/ViewModuleAttendance';
-import addOrUpdateModule from './components/admin/AddOrUpdateModule'
-import ViewDashboard from './components/lecturer/ViewDashboard';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FooterComponent from "./components/fragments/FooterComponent";
+import HeaderComponent from "./components/fragments/HeaderComponent";
+import LoginComponent from "./components/fragments/LoginComponent";
+import ListLecturer from "./components/admin/ListLecturer";
+import AddOrUpdateLecturer from "./components/admin/AddOrUpdateLecturer";
+import AddOrUpdateModule from "./components/admin/AddOrUpdateModule";
+import ListModule from "./components/admin/ListModule";
+import AdminMainPageComponent from "./components/admin/AdminMainPageComponent";
+import LecturerMainPage from "./components/lecturer/LecturerMainPage";
+import HomeComponent from "./components/fragments/HomeComponent";
+import ListStudent from "./components/admin/ListStudent";
+import ListClassSchedule from "./components/admin/ListClassSchedule";
+import ListClass from "./components/admin/ListClass";
+import ListStudentsLeave from "./components/admin/ListStudentsLeave";
+import ListModuleAttendence from "./components/admin/ListModuleAttendance";
+import ViewModuleStudentAttendance from "./components/admin/ViewModuleStudentAttendance";
+import ViewModuleAttendence from "./components/admin/ViewModuleAttendance";
+import addOrUpdateModule from "./components/admin/AddOrUpdateModule";
+import ViewDashboard from "./components/lecturer/ViewDashboard";
+import ListClassStudentAttendence from "./components/lecturer/ListClassStudentAttendence";
+import ListTeachingSchedule from "./components/lecturer/ListTeachingSchedule";
 
 function App() {
   return (
     <div>
       <Router>
-          <HeaderComponent/>
-          <div className="container">
-            <Switch> 
-              <Route path="/" exact component={HomeComponent}></Route>
-              <Route path="/admin/home" component={AdminMainPageComponent}></Route>
-              <Route path="/lecturer/home" component={LecturerMainPage}></Route>
-              <Route path="/login" component={LoginComponent}></Route>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={HomeComponent}></Route>
+            <Route
+              path="/admin/home"
+              component={AdminMainPageComponent}
+            ></Route>
+            <Route path="/lecturer/home" component={LecturerMainPage}></Route>
+            <Route path="/login" component={LoginComponent}></Route>
 
-              <Route path="/admin/lecturers" component={ListLecturer}></Route>
-              <Route path="/admin/add-lecturer/:id" component={AddOrUpdateLecturer}></Route>
-              <Route path="/admin/ListStudent" component={ListStudent}></Route>
+            <Route path="/admin/lecturers" component={ListLecturer}></Route>
+            <Route
+              path="/admin/add-lecturer/:id"
+              component={AddOrUpdateLecturer}
+            ></Route>
+            <Route path="/admin/ListStudent" component={ListStudent}></Route>
 
-              <Route path="/admin/module" component={ListModule}></Route>
-              <Route path="/admin/add-Module/:id" component={AddOrUpdateModule}></Route>
-              <Route path="/admin/module-attendance" component={ListModuleAttendence}></Route>
-              <Route path="/admin/module-classes-attendance/:id" component={ViewModuleAttendence}></Route>
-              <Route path="/admin/class-stuednts-attendance/:id" component={ViewModuleStudentAttendance}></Route>
+            <Route path="/admin/module" component={ListModule}></Route>
+            <Route
+              path="/admin/add-Module/:id"
+              component={AddOrUpdateModule}
+            ></Route>
+            <Route
+              path="/admin/module-attendance"
+              component={ListModuleAttendence}
+            ></Route>
+            <Route
+              path="/admin/module-classes-attendance/:id"
+              component={ViewModuleAttendence}
+            ></Route>
+            <Route
+              path="/admin/class-stuednts-attendance/:id"
+              component={ViewModuleStudentAttendance}
+            ></Route>
 
+            <Route
+              path="/lecturer/class-student-attendance"
+              component={ListClassStudentAttendence}
+            ></Route>
+            <Route
+              path="/lecturer/teaching-schedule"
+              component={ListTeachingSchedule}
+            ></Route>
+            <Route path="/lecturer/dashboard" component={ViewDashboard}></Route>
 
-              {/* these routes may need change when do the component coding*/}
-              <Route path="/admin/class-schedule" component={ListClassSchedule}></Route>
-              <Route path="/admin/Class" component={ListClass}></Route>
-              <Route path="/admin/student-leave-application" component={ListStudentsLeave}></Route>
-              
-              <Route path="/module" component={ListModule}></Route>
-              <Route path="/add-Module/:id" component={addOrUpdateModule}></Route>
-              <Route path="/lecturerDashboard" component={ViewDashboard}></Route>
-            </Switch>
-          </div>
-          <FooterComponent/>
+            {/* these routes may need change when do the component coding*/}
+            <Route
+              path="/admin/class-schedule"
+              component={ListClassSchedule}
+            ></Route>
+            <Route path="/admin/Class" component={ListClass}></Route>
+            <Route
+              path="/admin/student-leave-application"
+              component={ListStudentsLeave}
+            ></Route>
+
+            <Route path="/module" component={ListModule}></Route>
+            <Route path="/add-Module/:id" component={addOrUpdateModule}></Route>
+            {/* <Route path="/lecturerDashboard" component={ViewDashboard}></Route> */}
+          </Switch>
+        </div>
+        <FooterComponent />
       </Router>
     </div>
   );
--- src/components/lecturer/LecturerMainPage.js
+++ src/components/lecturer/LecturerMainPage.js
@@ -1,43 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import TeachingSchedule from '../media/8_lecturer_TeachingSchedule.png';
-import Attendance from '../media/9_lecturer_Attendance.png';
-import Dashboard from '../media/10_lecturer_Dashboard.png';
+import TeachingSchedule from "../media/8_lecturer_TeachingSchedule.png";
+import Attendance from "../media/9_lecturer_Attendance.png";
+import Dashboard from "../media/10_lecturer_Dashboard.png";
 
-import ListTeachingSchedule from './ListTeachingSchedule';
-import ListClassStudentAttendance from './ListClassStudentAttendence';
-import ViewDashboard from './ViewDashboard';
+import ListTeachingSchedule from "./ListTeachingSchedule";
+import ListClassStudentAttendance from "./ListClassStudentAttendence";
+import ViewDashboard from "./ViewDashboard";
 
 class LecturerMainPage extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container">
-            <div className="row align-items-center mt-4 d-flex justify-content-center row-cols-3">
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={ListTeachingSchedule} className="v-align"><img src={TeachingSchedule} alt="TeachingSchedule"  className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row align-items-center mt-4 d-flex justify-content-center row-cols-3">
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card icon white darken-1">
+                <a href={"./teaching-schedule"} className="v-align">
+                  <img
+                    src={TeachingSchedule}
+                    alt="TeachingSchedule"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={ListClassStudentAttendance} className="v-align"><img src={Attendance} alt="Attendance" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card icon white darken-1">
+                <a href={"./class-student-attendance"} className="v-align">
+                  <img
+                    src={Attendance}
+                    alt="Attendance"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={ViewDashboard} className="v-align"><img src={Dashboard} alt="Dashboard" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
-
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card icon white darken-1">
+                <a href={"./dashboard"} className="v-align">
+                  <img
+                    src={Dashboard}
+                    alt="Dashboard"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
             </div>
           </div>
-          
-            </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LecturerMainPage;
