4759c525eb4f897b3c54de2b6504277db5c59b39
larrysimm <sim.larry88@gmail.com>
2021-08-24 17:26:41 +0800

    Change the Welcome capitalization and the onclick to mainpage for the Home icon

--- src/components/fragments/HeaderComponent.js
+++ src/components/fragments/HeaderComponent.js
@@ -1,42 +1,54 @@
-import React, { Component } from 'react';
-import { renderIntoDocument } from 'react-dom/cjs/react-dom-test-utils.production.min';
-import logo from '../media/1_logo.png';
+import React, { Component } from "react";
+import { renderIntoDocument } from "react-dom/cjs/react-dom-test-utils.production.min";
+import logo from "../media/1_logo.png";
 
 class HeaderComponent extends Component {
-    logout = () => {
-        sessionStorage.removeItem("fullname");
-        sessionStorage.removeItem("token");
-        sessionStorage.removeItem("role");
-        window.location.href = "/"
-    }
+  logout = () => {
+    sessionStorage.removeItem("fullname");
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("role");
+    window.location.href = "/";
+  };
 
-    mainpage = () => {
-        let role = sessionStorage.getItem("role")
-        switch (role) {
-            case "admin":
-                window.location.href = "/admin/home";
-                break;
-            case "lecturer":
-                window.location.href = "/lecturer/home";
-                break;
-            default:
-                break;
-        }
+  mainpage = () => {
+    let role = sessionStorage.getItem("role");
+    switch (role) {
+      case "admin":
+        window.location.href = "/admin/home";
+        break;
+      case "lecturer":
+        window.location.href = "/lecturer/home";
+        break;
+      default:
+        break;
     }
+  };
 
-    render() {
-        return (
-            <div>
-                <header>
-                    <nav className="navbar navbar-dark header-navbar h-auto">
-                        <div><a href="https://www.nus.edu.sg/" className="navbar-brand"><img src={logo} alt="logo" height="60" /></a></div>
-                        {sessionStorage.getItem("fullname") && <div onClick={this.mainpage} ><h2>welcome! {sessionStorage.getItem("fullname")}</h2></div>}
-                        {sessionStorage.getItem("token") && <div onClick={this.logout} ><h4>logout</h4></div>}
-                    </nav>
-                </header>
+  render() {
+    return (
+      <div>
+        <header>
+          <nav className="navbar navbar-dark header-navbar h-auto">
+            <div onClick={this.mainpage}>
+              <a className="navbar-brand">
+                <img src={logo} alt="logo" height="60" />
+              </a>
             </div>
-        );
-    }
+            {sessionStorage.getItem("fullname") && (
+              <div>
+                <h2>Welcome! {sessionStorage.getItem("fullname")}</h2>
+              </div>
+            )}
+            {sessionStorage.getItem("token") && (
+              <div onClick={this.logout}>
+                <h4>logout</h4>
+              </div>
+            )}
+          </nav>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default HeaderComponent;
