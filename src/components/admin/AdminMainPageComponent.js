4be95347d2934cad7aac89f98dccecbab71889c5
larrysimm <sim.larry88@gmail.com>
2021-08-24 17:48:16 +0800

    Link the main page to the respective components

--- src/components/admin/AdminMainPageComponent.js
+++ src/components/admin/AdminMainPageComponent.js
@@ -1,60 +1,105 @@
-import React, { Component } from 'react';
-import Accounts from '../media/2_admin_Accounts.png';
-import Modules from '../media/3_admin_Modules.png';
-import ClassSchedule from '../media/4_admin_ClassSchedule.png';
-import Enrolments from '../media/5_admin_Enrolments.png';
-import Attendance from '../media/6_admin_Attendance.png';
-import StudentLeaveApplication from '../media/7_admin_StudentLeaveApplication.png';
-
+import React, { Component } from "react";
+import Accounts from "../media/2_admin_Accounts.png";
+import Modules from "../media/3_admin_Modules.png";
+import ClassSchedule from "../media/4_admin_ClassSchedule.png";
+import Enrolments from "../media/5_admin_Enrolments.png";
+import Attendance from "../media/6_admin_Attendance.png";
+import StudentLeaveApplication from "../media/7_admin_StudentLeaveApplication.png";
 
 class AdminMainPageComponent extends Component {
-    render() {
-        return (
-            <div>
-               
-          <div className="container">
-            <div className="row align-items-center mt-4 d-inline-flex justify-content-center row-cols-3">
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={"./students"} className="v-align"><img src={Accounts} alt="Accounts" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row align-items-center mt-4 d-inline-flex justify-content-center row-cols-3">
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./students"} className="v-align">
+                  <img
+                    src={Accounts}
+                    alt="Accounts"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={"./ListModule"} className="v-align"><img src={Modules} alt="Modules" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./ListModule"} className="v-align">
+                  <img
+                    src={Modules}
+                    alt="Modules"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={"./ListClassSchedule"} className="v-align"><img src={ClassSchedule} alt="ClassSchedule" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./ListClassSchedule"} className="v-align">
+                  <img
+                    src={ClassSchedule}
+                    alt="ClassSchedule"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={"./ListClass"} className="v-align"><img src={Enrolments} alt="Enrolments" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
-                
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={"./ListAttendance"} className="v-align" ><img src={Attendance} alt="Attendance" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./ListClass"} className="v-align">
+                  <img
+                    src={Enrolments}
+                    alt="Enrolments"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth" >
-                <div className="card white darken-1">
-                <a href={"./StudentLeaveApplication"} className="v-align" ><img src={StudentLeaveApplication} alt="StudentLeaveApplication" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./module-attendance"} className="v-align">
+                  <img
+                    src={Attendance}
+                    alt="Attendance"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
+
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./StudentLeaveApplication"} className="v-align">
+                  <img
+                    src={StudentLeaveApplication}
+                    alt="StudentLeaveApplication"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
             </div>
           </div>
-          
-            </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AdminMainPageComponent;
--- src/components/lecturer/LecturerMainPage.js
+++ src/components/lecturer/LecturerMainPage.js
@@ -1,63 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import TeachingSchedule from '../media/8_lecturer_TeachingSchedule.png';
-import Attendance from '../media/9_lecturer_Attendance.png';
-import Dashboard from '../media/10_lecturer_Dashboard.png';
+import TeachingSchedule from "../media/8_lecturer_TeachingSchedule.png";
+import Attendance from "../media/9_lecturer_Attendance.png";
+import Dashboard from "../media/10_lecturer_Dashboard.png";
 
-import ListTeachingSchedule from './ListTeachingSchedule';
-import ListClassStudentAttendance from './ListClassStudentAttendence';
-import ViewDashboard from './ViewDashboard';
-import LecturerService from '../../services/LecturerService';
+import ListTeachingSchedule from "./ListTeachingSchedule";
+import ListClassStudentAttendance from "./ListClassStudentAttendence";
+import ViewDashboard from "./ViewDashboard";
+import LecturerService from "../../services/LecturerService";
 
 class LecturerMainPage extends Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            id:[]
-        }          
-        
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: [],
+    };
+  }
 
-    componentDidMount(){
-        LecturerService.getmaxID().then((response) =>{
-            this.setState({id:response.data})
-        })    
-    }
-    
+  componentDidMount() {
+    LecturerService.getmaxID().then((response) => {
+      this.setState({ id: response.data });
+    });
+  }
 
+  render() {
+    const id = this.state.id.maxID;
+    const myLink = `/lecturer/overview/${id}`;
+    const dashboardLink = `/lecturer/dashboard/0`;
+    return (
+      <div>
+        <div className="container">
+          <div className="row align-items-center mt-4 d-flex justify-content-center row-cols-3">
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={"./schedules"} className="v-align">
+                  <img
+                    src={TeachingSchedule}
+                    alt="TeachingSchedule"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-    render() {
-        const id= this.state.id.maxID;
-        const myLink = `/lecturer/overview/${id}`;
-        const dashboardLink = `/lecturer/dashboard/0`
-        return (
-            <div>
-                <div className="container">
-            <div className="row align-items-center mt-4 d-flex justify-content-center row-cols-3">
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={ListTeachingSchedule} className="v-align"><img src={TeachingSchedule} alt="TeachingSchedule"  className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={myLink} className="v-align">
+                  <img
+                    src={Attendance}
+                    alt="Attendance"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
+            </div>
 
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href= {myLink} className="v-align"><img src={Attendance} alt="Attendance" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
-
-                <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
-                <div className="card white darken-1">
-                <a href={dashboardLink} className="v-align"><img src={Dashboard} alt="Dashboard" className="img-fluid" height="200" width="200"/></a>
-                </div>
-                </div>
-
+            <div className="col col-lg-3 d-inline-flex justify-content-center m-1 customMinWidth">
+              <div className="card white darken-1">
+                <a href={dashboardLink} className="v-align">
+                  <img
+                    src={Dashboard}
+                    alt="Dashboard"
+                    className="img-fluid"
+                    height="200"
+                    width="200"
+                  />
+                </a>
+              </div>
             </div>
           </div>
-          
-            </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LecturerMainPage;
