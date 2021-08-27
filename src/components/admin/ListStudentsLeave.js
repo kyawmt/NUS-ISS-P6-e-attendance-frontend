4b2b59e1ef9a8c5973475a28febec6873f23b7b5
e0696894 <e0696894@u.nus.edu>
2021-08-27 15:46:42 +0800

    Update ListStudentsLeave.js

--- src/components/admin/ListStudentsLeave.js
+++ src/components/admin/ListStudentsLeave.js
@@ -4,7 +4,7 @@ class ListStudentsLeave extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Coming Soon...</h1>
             </div>
         );
     }
