04c1043942f67e7ac48902e164f3ebfc5b8f156a
kyawmt <kmt@kmtkko.com>
2021-08-28 10:53:21 +0800

    For deployment
    (cherry picked from commit 8c401e1a400394a0aca0539626a06ad8da722ccf)
    (cherry picked from commit e1a56b95e6dcd1ecd9dc2632af544d25e88561c1)

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://sa52-e-attendance-backend.herokuapp.com"
 })
 
 axiosInstance.interceptors.request.use(config => {
